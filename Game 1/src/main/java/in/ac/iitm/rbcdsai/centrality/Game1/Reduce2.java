c07ec47dbaecf57310068d2a492e7706e300d866
Synchon Mandal <synchonmandal@gmail.com>
2018-12-11 13:53:30 +0530

    chore: update .gitignore; update: add Game 1

---
+++ Game 1/src/main/java/in/ac/iitm/rbcdsai/centrality/Game1/Reduce2.java
@@ -0,0 +1,38 @@
+package in.ac.iitm.rbcdsai.centrality.Game1;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+/**
+ * Represents a Reduce2 class required for second stage of Reduce for Game1.
+ * 
+ * @author M Vishnu Sankar
+ * @version 1.0
+ */
+public class Reduce2 extends Reducer<Text, FloatWritable, Text, FloatWritable> {
+
+    /**
+     * Provides functionality for Reduce.
+     *
+     * @param  key                 the key
+     * @param  values              iterable of FloatWritable
+     * @param  context             the context
+     * @throws IOException         if IOException occured
+     * @throws InteruptedException if InterruptedExecution occured
+     */
+	public void reduce(Text key, Iterable<FloatWritable> values, Context context)
+        throws IOException, InterruptedException {
+
+		float centrality = 0;
+
+	    for (FloatWritable val : values) {
+	    	centrality += val.get();
+	    }
+
+		context.write(key,new FloatWritable(centrality));
+	}
+}
